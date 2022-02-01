f8924de2048c88098a0e046c9776c58073b67df0
Ntiri Boateng <ntiriboateng.nb@gmail.com>
2022-02-01 13:56:29 +0000

    Errors fixed

--- run_analysis.R
+++ run_analysis.R
@@ -21,7 +21,7 @@ Dataset_X_Train <- read.table('./UCI HAR Dataset/train/X_train.txt')
 Dataset_Train_Combine <- data.frame(Dataset_Subject_Train, Dataset_Y_Train, Dataset_X_Train)
 
 #Giving names to the elements in the Train combined data set
-names(Dataset_Train_Combine) <- c('Subject','Activity', 'Features')
+names(Dataset_Train_Combine) <- c(c('Subject', 'Activity'), Dataset_features)
 
 #Reading and Converting the Data set for Test
 Dataset_Subject_Test <- read.csv('./UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = ' ')
@@ -32,7 +32,7 @@ Dataset_X_Test <- read.table('./UCI HAR Dataset/test/X_test.txt')
 Dataset_Test_Combine <-  data.frame(Dataset_Subject_Test, Dataset_Y_Test, Dataset_X_Test)
 
 #Giving names to the elements in the Train combined data set
-names(Dataset_Test_Combine) <- c('Subject','Activity', 'Features')
+names(Dataset_Test_Combine) <- c(c('Subject', 'Activity'), Dataset_features)
 
 
 #Merges the train and the test sets to create one data set
@@ -44,14 +44,17 @@ columnsWithMeanSTD  <- grep('mean|std', Dataset_features)
 data_replace <- Combine_Train_and_Test_Dataset[,c(1,2,columnsWithMeanSTD + 2)]
 
 # Question 3 - Use descriptive activity names to name the activities in the data set
-Data_activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+Data_activity_labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header=FALSE)
 Data_activity_labels<- as.character(Data_activity_labels[,2])
 
 data_replace$Activity <- Data_activity_labels[data_replace$Activity]
 
+print(data_replace)
+
 
 # Question 4 - Appropriately labels the data set with descriptive variable names.
 New_Name <- names(data_replace)
+New_Name <- gsub("[(][)]", "", New_Name)
 New_Name<-gsub("Acc", "Accelerometer", New_Name)
 New_Name<-gsub("Gyro", "Gyroscope", New_Name)
 New_Name<-gsub("BodyBody", "Body", New_Name)
@@ -64,14 +67,14 @@ New_Name<-gsub("-std()", "STD", New_Name, ignore.case = TRUE)
 New_Name<-gsub("-freq()", "Frequency", New_Name, ignore.case = TRUE)
 New_Name<-gsub("angle", "Angle", New_Name)
 New_Name<-gsub("gravity", "Gravity", New_Name)
-
+New_Name <- gsub("-", "_", New_Name)
 names(data_replace) <- New_Name
 
+names(data_replace)
 
 # Question 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
 Tidy_Data <- aggregate(data_replace[,3:81], by = list(activity = data_replace$Activity, subject = data_replace$Subject),FUN = mean)
 write.table(x = Tidy_Data, file = "./data/tidy_data.txt", row.names = FALSE)
-
 
 
 
